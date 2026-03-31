0f6cf68d84f483c01ca86e62154198e5dff63fc2
slingb1ade <86094891+slingb1ade@users.noreply.github.com>
2026-03-31 21:59:54 +1300

    Add files via upload

--- app.py
+++ app.py
@@ -168,4 +168,4 @@ def metar_proxy(station):
         return jsonify([]), 500
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
